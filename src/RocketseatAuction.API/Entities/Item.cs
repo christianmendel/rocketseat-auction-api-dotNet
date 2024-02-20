f2a731e795707e7fbee9a32a8d0753671c8989ac
Christian Diemer <diemerc@delagelanden.com>
2024-02-20 10:44:18 -0300

    v1

---
+++ src/RocketseatAuction.API/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using RocketseatAuction.API.Enums;
+
+namespace RocketseatAuction.API.Entities;
+
+[Table("Items")]
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public Condition Condition { get; set; }
+    public decimal BasePrice { get; set; }
+    public int AuctionId { get; set; }
+
+}
---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    private readonly IAuctionRepository _repository;
+
+    public GetCurrentAuctionUseCase(IAuctionRepository repository) => _repository = repository;
+
+    public Auction? Execute() => _repository.GetCurrent();
+}
